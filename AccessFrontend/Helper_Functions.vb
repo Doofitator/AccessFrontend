9979fe9b075a14ded084855a6888c4b219cffa15
Doofitator <ashley@sharkey.id.au>
2018-10-26 15:15:09 +1100

    Fixed last commit issue with try/catch like a boss

--- AccessFrontend/Helper_Functions.vb
+++ AccessFrontend/Helper_Functions.vb
@@ -49,50 +49,55 @@
     End Function
 
     Public Function resizeControlsGroupboxContent(ByVal isReadPage As Boolean)
-        If isReadPage Then
-            If frm_main.tab_controls.Height > 370 Then
-                frm_main.tab_results.Width = frm_main.tab_controls.Width - 30
-                frm_main.tab_results.Left = 10
-                frm_main.tab_results.Top = 231
-                frm_main.tab_results.Height = frm_main.tab_controls.Height - 276
-                frm_main.lbl_resultsHint.Top = 210
-                frm_main.lbl_resultsHint.Left = 10
+        Try
+            If isReadPage Then
+                If frm_main.tab_controls.Height > 370 Then
+                    frm_main.tab_results.Width = frm_main.tab_controls.Width - 30
+                    frm_main.tab_results.Left = 10
+                    frm_main.tab_results.Top = 231
+                    frm_main.tab_results.Height = frm_main.tab_controls.Height - 276
+                    frm_main.lbl_resultsHint.Top = 210
+                    frm_main.lbl_resultsHint.Left = 10
+                Else
+                    frm_main.tab_results.Left = 551
+                    frm_main.tab_results.Width = frm_main.tab_controls.Width - frm_main.lbl_osParent.Width - frm_main.lbl_osBoot.Width - frm_main.cbx_osBoot.Width - frm_main.cbx_osParent.Width - 250
+                    frm_main.tab_results.Top = 38
+                    frm_main.tab_results.Height = 137
+                    frm_main.lbl_resultsHint.Left = 551
+                    frm_main.lbl_resultsHint.Top = 7
+                End If
+
+                frm_main.txt_Features.Height = frm_main.tpg_Features.Height - 14
+                frm_main.txt_Notes.Height = frm_main.tpg_Notes.Height - 14
+                frm_main.txt_Features.Width = frm_main.tpg_Features.Width - 14
+                frm_main.txt_Notes.Width = frm_main.tpg_Features.Width - 14
+                frm_main.txt_specs.Width = frm_main.tpg_Features.Width - 14
+                frm_main.txt_specs.Height = frm_main.tpg_Features.Height - 14
             Else
-                frm_main.tab_results.Left = 551
-                frm_main.tab_results.Width = frm_main.tab_controls.Width - frm_main.lbl_osParent.Width - frm_main.lbl_osBoot.Width - frm_main.cbx_osBoot.Width - frm_main.cbx_osParent.Width - 250
-                frm_main.tab_results.Top = 38
-                frm_main.tab_results.Height = 137
-                frm_main.lbl_resultsHint.Left = 551
-                frm_main.lbl_resultsHint.Top = 7
+                If frm_main.tab_controls.Height > 370 Then
+                    frm_main.tab_ExtrasEdit.Width = frm_main.tab_controls.Width - 30
+                    frm_main.tab_ExtrasEdit.Left = 10
+                    frm_main.tab_ExtrasEdit.Top = 201
+                    frm_main.tab_ExtrasEdit.Height = frm_main.tpg_edit.Height - 249
+                Else
+                    frm_main.tab_ExtrasEdit.Left = 374
+                    frm_main.tab_ExtrasEdit.Width = frm_main.tab_controls.Width - frm_main.lbl_osParentEdit.Width - frm_main.lbl_osBootEdit.Width - frm_main.cbx_osBootEdit.Width - frm_main.cbx_osParentEdit.Width - 70
+                    frm_main.tab_ExtrasEdit.Top = 12
+                    frm_main.tab_ExtrasEdit.Height = 137
+                End If
+
+                frm_main.txt_FeaturesEdit.Height = frm_main.tpg_FeaturesEdit.Height - 14
+                frm_main.txt_NotesEdit.Height = frm_main.tpg_notesEdit.Height - 14
+                frm_main.txt_FeaturesEdit.Width = frm_main.tpg_FeaturesEdit.Width - 14
+                frm_main.txt_NotesEdit.Width = frm_main.tpg_FeaturesEdit.Width - 14
+                frm_main.btn_Save.Left = frm_main.tpg_edit.Width - frm_main.btn_Save.Width - 10
+                frm_main.btn_Save.Top = frm_main.tab_ExtrasEdit.Height + frm_main.tab_ExtrasEdit.Top + 10
             End If
 
-            frm_main.txt_Features.Height = frm_main.tpg_Features.Height - 14
-            frm_main.txt_Notes.Height = frm_main.tpg_Notes.Height - 14
-            frm_main.txt_Features.Width = frm_main.tpg_Features.Width - 14
-            frm_main.txt_Notes.Width = frm_main.tpg_Features.Width - 14
-            frm_main.txt_specs.Width = frm_main.tpg_Features.Width - 14
-            frm_main.txt_specs.Height = frm_main.tpg_Features.Height - 14
-        Else
-            If frm_main.tab_controls.Height > 370 Then
-                frm_main.tab_ExtrasEdit.Width = frm_main.tab_controls.Width - 30
-                frm_main.tab_ExtrasEdit.Left = 10
-                frm_main.tab_ExtrasEdit.Top = 201
-                frm_main.tab_ExtrasEdit.Height = frm_main.tpg_edit.Height - 249
-            Else
-                frm_main.tab_ExtrasEdit.Left = 374
-                frm_main.tab_ExtrasEdit.Width = frm_main.tab_controls.Width - frm_main.lbl_osParentEdit.Width - frm_main.lbl_osBootEdit.Width - frm_main.cbx_osBootEdit.Width - frm_main.cbx_osParentEdit.Width - 70
-                frm_main.tab_ExtrasEdit.Top = 12
-                frm_main.tab_ExtrasEdit.Height = 137
-            End If
-
-            frm_main.txt_FeaturesEdit.Height = frm_main.tpg_FeaturesEdit.Height - 14
-            frm_main.txt_NotesEdit.Height = frm_main.tpg_notesEdit.Height - 14
-            frm_main.txt_FeaturesEdit.Width = frm_main.tpg_FeaturesEdit.Width - 14
-            frm_main.txt_NotesEdit.Width = frm_main.tpg_FeaturesEdit.Width - 14
-            frm_main.btn_Save.Left = frm_main.tpg_edit.Width - frm_main.btn_Save.Width - 10
-            frm_main.btn_Save.Top = frm_main.tab_ExtrasEdit.Height + frm_main.tab_ExtrasEdit.Top + 10
-        End If
-
-        Return True
+            Return True
+        Catch
+            Return False
+            MsgBox("Resize error. Please try again.", vbInformation + vbOKOnly, "Whoops")
+        End Try
     End Function
 End Module
